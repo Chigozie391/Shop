92c477289c86a810ead3454639c5c5e7cf8f2712
Chigozie <chigoziemadubuko@gmail.com>
2018-12-12 22:35:44 +0100

    Added Sold Column

---
+++ Shop.API/Migrations/20181212160216_SoldColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class SoldColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sold",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sold",
+                table: "Products");
+        }
+    }
+}
